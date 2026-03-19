31053c03fe9fc977f390f559dbb20eddb6dc78c3
Priyanka <priyanka.shakya15@gmail.com>
2026-03-19 11:51:12 +1100

    input of max 100

--- src/prime_number.py
+++ src/prime_number.py
@@ -4,9 +4,16 @@
         • the sum of all prime numbers found
 """
 def find_all_prime_numbers(user_input: str) -> str:
-    """ Code for question 2 goes here """
-    return "True"
+
+
+   
+   
+ return "True"
 
 if __name__ == "__main__":
-    user_input = input("Enter a number: ")
-    find_all_prime_numbers(user_input)
+    user_input = int(input("Enter a number (max 100): "))
+
+    if user_input > 100:
+        print("Please enter a number less than or equal to 100.")
+    else:
+        find_all_prime_numbers(user_input)
