2d03cfecf885276b412af77b177b0f8bbe29c826
nahida <nahida.zh7@gmail.com>
2026-03-20 15:55:57 +1100

    My final code for task 1

--- src/strength_check.py
+++ src/strength_check.py
@@ -1,24 +1,15 @@
 import re
-"""
-    Checks if the given password is strong.
-    Criteria:
-    - At least 8 characters
-    - Contains uppercase, lowercase, digit, and special character
-"""
-def is_strong_password(password: str) -> str:
-    """ if len(password) < 8:
-        return False
-    if not re.search(r'[A-Z]', password):
-        return False
-    if not re.search(r'[a-z]', password):
-        return False
-    if not re.search(r'\\d', password):
-        return False
-    if not re.search(r'[^A-Za-z0-9]', password):
-        return False
-    return True """
-    return "False"
+password = input("Enter your password: ")
 
-if __name__ == "__main__":
-    password = input("Enter a password: ")
-    is_strong_password(password)
+length = len(password)
+has_digit = any(char.isdigit() for char in password)
+has_upper = any(char.isupper() for char in password)
+
+if length < 6:
+    print("Weak password")
+elif 6 <= length <= 10 and has_digit:
+    print("Medium password")
+elif length > 10 and has_digit and has_upper:
+    print("Strong password")
+else:
+    print("Weak password")
